2a8f43bd7718d5369f60a835450b51ba5538c033
helenyjx <112274822+helenyjx@users.noreply.github.com>
2022-09-29 17:31:15 +0000

    update

--- repeat.sh
+++ repeat.sh
@@ -10,22 +10,31 @@ sort_record Walmart.csv
 
 #!/bin/bash
 # Updating the record after excluding the header line from the output.
-while read line
-do
-   echo "Record is : $line"
-done < <(tail -n +2 Walmart.csv)
+sort_record_no_header() {
+    while read line
+    do
+      echo "Record is : $line"
+    done < <(tail -n +2 $1)
+}
+sort_record_no_header Walmart.csv
 
 #! /bin/bash
 # Read the values from each data record.
-while IFS="," read -r rec_column1 rec_column2 rec_column3 rec_column4 rec_column5 rec_column6 rec_column7 rec_column8
-do
-  echo "Displaying Record for Store-$rec_column1"
-  echo "Date: $rec_column2"
-  echo "Weekly Sales: $rec_column3"
-  echo "Holiday(1) or Non-holiday (0): $rec_column4"
-  echo "Temperature: $rec_column5"
-  echo "Fuel Price: $rec_column6"
-  echo "CPI: $rec_column7"
-  echo "Unemployment: $rec_column8"
-  echo ""
-done < <(tail -n +2 Walmart.csv)
+read_data() {
+    while IFS="," read -r rec_column1 rec_column2 rec_column3 rec_column4 rec_column5 rec_column6 rec_column7 rec_column8
+    do
+      echo "Displaying Record for Store-$rec_column1"
+      echo "Date: $rec_column2"
+      echo "Weekly Sales: $rec_column3"
+      echo "Holiday(1) or Non-holiday (0): $rec_column4"
+      echo "Temperature: $rec_column5"
+      echo "Fuel Price: $rec_column6"
+      echo "CPI: $rec_column7"
+      echo "Unemployment: $rec_column8"
+      echo ""
+    done < <(tail -n +2 $1)
+}
+read_data Walmart.csv
+
+
+
--- sort.sh
+++ sort.sh
@@ -5,3 +5,18 @@ repeat() {
 }
 
 repeat $1 $2
+
+#! /bin/bash
+# Read the values from each data record.
+while IFS="," read -r rec_column1 rec_column2 rec_column3 rec_column4 rec_column5 rec_column6 rec_column7 rec_column8
+do
+  echo "Displaying Record for Store-$rec_column1"
+  echo "Date: $rec_column2"
+  echo "Weekly Sales: $rec_column3"
+  echo "Holiday(1) or Non-holiday (0): $rec_column4"
+  echo "Temperature: $rec_column5"
+  echo "Fuel Price: $rec_column6"
+  echo "CPI: $rec_column7"
+  echo "Unemployment: $rec_column8"
+  echo ""
+done < <(tail -n +2 Walmart.csv)
